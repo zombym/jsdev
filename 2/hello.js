656cfb36ca70148bf3c4e833a0b9fbc3fbf71035
Александр Пономарев <aponomarev@tpar.local>
2018-06-05 16:55:17 +0300

    check

--- 2/hello.js
+++ 2/hello.js
@@ -1,4 +1,27 @@
-var myName = prompt('Укажите ваше имя');
-var mySurname = prompt('Укажите вашу фамилию');
+var a = prompt('Укажите первое число');
+var b = prompt('Укажите второе число');
+var summ = 0;
 
-alert('Привет ' + myName + ' ' + mySurname);
+if( a > b ) alert('Первое число больше второго');
+if( a < b ) alert('Второе число больше первого');
+if( a == b ) alert('Числа равны');
+
+var yearStart = prompt('Укажите первый год');
+var yearFinish = prompt('Укажите последний год');
+
+if(yearStart <= yearFinish){
+for( var i = yearStart; i <= yearFinish; i++)
+  if(i%4 == 0) console.log(i)
+}
+else{
+  for( var i = yearFinish; i <= yearStart; i++)
+    if(i%4 == 0) console.log(i)
+}
+
+do{
+  var c = prompt('Укажите число');
+  summ = summ + Number(c);
+  if(c===null) break;
+}while(c != 3)
+
+alert(summ);
