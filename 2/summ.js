751d8ef934333382629d3b10820c15970beb9506
Александр Пономарев <aponomarev@tpar.local>
2018-06-07 13:40:05 +0300

    correct

--- 2/summ.js
+++ 2/summ.js
@@ -2,8 +2,10 @@ var summ = 0;
 
 do{
   var c = prompt('Укажите число');
-  if(+c) summ = summ + Number(c);
   if(c===null) break;
+  if(+c) summ = summ + Number(c);
+    else alert('неверное значение');
+
 }while(true)
 
 alert(summ);
