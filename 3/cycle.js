8a9f08cd0f1b99d71215c823270af2bf52326827
Александр Пономарев <aponomarev@tpar.local>
2018-06-15 18:00:14 +0300

    prep

--- 3/cycle.js
+++ 3/cycle.js
@@ -1,8 +1,18 @@
 var rnd = Math.round(Math.random() * 1000);
 console.log(rnd);
+
+function checkInput(input) {
+  if(input===null) return 0;
+  if(!+input) return 1;
+}
+
 do{
-  var answer = prompt('укажите число');
-  if(+answer > rnd) alert ('больше');
-  if(+answer < rnd) alert('меньше');
+  var answer = prompt('Укажите число');
+  if(checkInput(answer) == 0) break;
+  else if (checkInput(answer) != 1){
+  if(+answer > rnd) alert ('Меньше!');
+  if(+answer < rnd) alert('Больше!');
+} else alert('Введи число!')
 }while (+answer !== rnd);
-alert('congrats');
+
+alert('Правильно!');
--- 3/rec.js
+++ 3/rec.js
@@ -1,12 +1,24 @@
 var rnd = Math.round(Math.random() * 1000);
 console.log(rnd);
 
+function checkInput(input) {
+  if(input===null) return 0;
+  if(!+input) return 1;
+}
+
 function chkRnd(rnd) {
-  var answer = prompt('укажите число');
+  var answer = prompt('Укажите число');
+  if(checkInput(answer) == 0) answer = rnd;
+
   if(+answer == rnd) return;
-  else chkRnd(rnd);
+  else if (checkInput(answer) != 1){
+    if(+answer > rnd) alert ('Меньше!');
+    if(+answer < rnd) alert('Больше!');
+    chkRnd(rnd);
+  } else {alert('Введи число!');
+          chkRnd(rnd);
+        };
 }
 
 chkRnd(rnd);
-
-alert('congrats');
+alert('Правильно!');
