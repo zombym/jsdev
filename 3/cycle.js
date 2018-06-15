b1c4ce1aa408bf743a55bfb34838e59031518d7e
zombym <zombymaster@gmail.com>
2018-06-15 22:26:45 +0300

    ok

--- 3/cycle.js
+++ 3/cycle.js
@@ -1,18 +1,10 @@
 var rnd = Math.round(Math.random() * 1000);
 console.log(rnd);
-
-function checkInput(input) {
-  if(input===null) return 0;
-  if(!+input) return 1;
-}
-
 do{
   var answer = prompt('Укажите число');
-  if(checkInput(answer) == 0) break;
-  else if (checkInput(answer) != 1){
+  if(answer===null) break;
+  if(!+answer) alert('Введи число!');
+  if(+answer == rnd) alert('Правильно!');
   if(+answer > rnd) alert ('Меньше!');
   if(+answer < rnd) alert('Больше!');
-} else alert('Введи число!')
 }while (+answer !== rnd);
-
-alert('Правильно!');
--- 3/rec.js
+++ 3/rec.js
@@ -1,24 +1,15 @@
 var rnd = Math.round(Math.random() * 1000);
 console.log(rnd);
 
-function checkInput(input) {
-  if(input===null) return 0;
-  if(!+input) return 1;
-}
-
 function chkRnd(rnd) {
   var answer = prompt('Укажите число');
-  if(checkInput(answer) == 0) answer = rnd;
+  if(answer === null) return;
+  if(!+answer) alert('Введи число!');
+  if(+answer > rnd) alert ('Меньше!');
+  if(+answer < rnd) alert('Больше!');
+  if(+answer == rnd) alert('Правильно!');
 
-  if(+answer == rnd) return;
-  else if (checkInput(answer) != 1){
-    if(+answer > rnd) alert ('Меньше!');
-    if(+answer < rnd) alert('Больше!');
-    chkRnd(rnd);
-  } else {alert('Введи число!');
-          chkRnd(rnd);
-        };
+  if(+answer !== rnd) chkRnd(rnd);
 }
 
 chkRnd(rnd);
-alert('Правильно!');
