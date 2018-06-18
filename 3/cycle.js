5cb20c81e413751220dddffa7c46d5fea60bd57a
Александр Пономарев <aponomarev@tpar.local>
2018-06-18 16:39:39 +0300

    ok

--- 3/cycle.js
+++ 3/cycle.js
@@ -1,10 +1,14 @@
 var rnd = Math.round(Math.random() * 1000);
 console.log(rnd);
-do{
+
+var i = 0;
+for(var i = 0; i < 5; i++)
+{
   var answer = prompt('Укажите число');
   if(answer===null) break;
   if(!+answer) alert('Введи число!');
   if(+answer == rnd) alert('Правильно!');
   if(+answer > rnd) alert ('Меньше!');
   if(+answer < rnd) alert('Больше!');
-}while (+answer !== rnd);
+  alert('Осталось попыток ' + (4 - i));
+}
--- 3/rec.js
+++ 3/rec.js
@@ -1,6 +1,8 @@
 var rnd = Math.round(Math.random() * 1000);
 console.log(rnd);
 
+var i = 0;
+
 function chkRnd(rnd) {
   var answer = prompt('Укажите число');
   if(answer === null) return;
@@ -8,7 +10,9 @@ function chkRnd(rnd) {
   if(+answer > rnd) alert ('Меньше!');
   if(+answer < rnd) alert('Больше!');
   if(+answer == rnd) alert('Правильно!');
-
+  i++;
+  alert('Осталось попыток ' + (5 - i));
+  if(i == 5) return;
   if(+answer !== rnd) chkRnd(rnd);
 }
 
